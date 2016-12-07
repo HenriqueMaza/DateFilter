0e447f785e892a7dd509b0dc530bcc26527e9a66
HenriqueMaza <henriqueraraujo@gmail.com>
2016-12-07 15:17:25 -0200

    change to factory

--- utilities.js
+++ utilities.js
@@ -1,14 +1,13 @@
-angular.module('utilities', [])
-       .factory('utilities', utilities)
+App.factory('$utilities', function () {
 
-function mylib () {
-  var exports = {};
+    return new function () {
 
-  exports.formataDataParaOrdenacao = function (data){
-  var dataParaFormatacao = data;
-  var pattern = /(\d{2})(\d{2})(\d{4})(\d{2})(\d{2})/;
-  dataParaFormatacao = dataParaFormatacao.replace("/","").replace("/","").replace(" ", "").replace(" ", "").replace(":","");
-  return dataParaFormatacao.replace(pattern, '$3-$2-$1T$4:$5');
-  };
-  return exports;
-}
+        this.formataDataParaOrdenacao = function (data) {
+            var dataParaFormatacao = data;
+            var pattern = /(\d{2})(\d{2})(\d{4})(\d{2})(\d{2})/;
+            dataParaFormatacao = dataParaFormatacao.replace("/", "").replace("/", "").replace(" ", "").replace(" ", "").replace(":", "");
+            return dataParaFormatacao.replace(pattern, '$3-$2-$1T$4:$5');
+        };
+    }
+    
+});
